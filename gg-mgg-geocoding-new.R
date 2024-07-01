5e50b2642135dccf730acbfa9021ddbed42c4454
Cameron Blevins <cameron.blevins@ucdenver.edu>
2024-07-01 14:58:18 -0600

    Cleaning up code
    Changed the code to series of functions, added some sub-folders to organize the various data, processing, geocoding files.

--- gg-mgg-geocoding-new.R
+++ gg-mgg-geocoding-new.R
@@ -4,65 +4,108 @@ library(ggmap)
 library(ggplot2)
 
 ### Read in Datasets
-columns <- c("title", "description", "type", "city", "state", "country", "publication", "year", "notes")
-all.data <- data.frame()
+
+## Initialize an empty dataframe to store all data
+empty.df <- data.frame(
+  title = character(),
+  description = character(),
+  type = character(),
+  city = character(),
+  state = character(),
+  country = character(),
+  publication = character(),
+  year = numeric(),
+  notes = character(),
+  stringsAsFactors = FALSE # This ensures that string data does not get converted to factors
+)
+columns <- colnames(empty.df)
+years <- c(1981, 1983)
 
 ## Read in Gaia's Guide data
-# Get a list of files in the GG-Data subfolder that match the "gg-XXXX.csv" pattern
-files <- list.files(path = "GG-Data", pattern = "gg-\\d{4}\\.csv$", full.names = FALSE)
-years <- gsub("gg-(\\d{4})\\.csv", "\\1", files) # Extract the year from each filename
-years <- as.numeric(years) # Convert the years to numeric
-years <- unique(years) # Get a list of unique years
-# Loop through each unique year in the subfolder to open them
-for (year in years) {
-  gg.filename <- paste("GG-Data/gg-", year, ".csv", sep = "")
-  print(paste("Reading in ", gg.filename, sep = ""))
-  #load data and add a publication and country column to match the other datasets
-  gg.data <- read.csv(gg.filename, header = TRUE)
-  gg.data$publication <- "Gaia's Guide"
-  gg.data$country <- "United States"
-  gg.data <- select(gg.data, intersect(columns, names(gg.data)))
-  all.data <- rbind(all.data, gg.data)
+load_gg_data <- function(df, columns, years) {
+  # Get a list of files in the GG-Data subfolder that match the "gg-XXXX.csv" pattern
+  #files <- list.files(path = "GG-Data", pattern = "gg-\\d{4}\\.csv$", full.names = FALSE)
+  #years <- gsub("gg-(\\d{4})\\.csv", "\\1", files) # Extract the year from each filename
+  #years <- as.numeric(years) # Convert the years to numeric
+  #years <- unique(years) # Get a list of unique years
+
+  # Loop through each unique year in the subfolder to open them
+  for (year in years) {
+    gg.filename <- file.path("GG-Data", paste("gg-", year, ".csv", sep = ""))
+    print(paste("Reading in ", gg.filename, sep = ""))
+    #load data and add a publication and country column to match the other datasets
+    gg.data <- read.csv(gg.filename, header = TRUE)
+    gg.data$publication <- "Gaia's Guide"
+    gg.data$country <- "United States"
+    gg.data <- select(gg.data, intersect(columns, names(gg.data)))
+    df <- rbind(df, gg.data)
+  }
+  return(df)
 }
 
-## Read in MGG Data, filter it by year and add to all.data
-mgg.data <- readRDS("MGG-Data/mgg-data-cleaned.rds")
-mgg.data <- mgg.data %>% rename(year = Year)
-mgg.data <- select(mgg.data, intersect(columns, names(mgg.data)))
-mgg.data <- mgg.data %>% filter(year %in% completed_years)
-all.data <- rbind(all.data, mgg.data)
+## Read in MGG Data, filter it by year to match the years in the Gaia's Guide data
+load_mgg_data<- function(columns){
+  mgg.data <- readRDS(file.path("MGG-Data", "mgg-data-cleaned.rds"))
+  mgg.data <- mgg.data %>% rename(year = Year)
+  mgg.data <- select(mgg.data, intersect(columns, names(mgg.data)))
+  return(mgg.data)
+}
+
+## call all data loading functions
+gg.data <- load_gg_data(empty.df, columns, years)
+mgg.data<-load_mgg_data(columns) %>%
+  filter(year %in% years)
+all.data <- rbind(gg.data, mgg.data)
+write.csv(all.data, file.path("data-processing-files", "all-data-precleaned.csv"), row.names = FALSE)
 
 ### Data Cleaning and Processing
 
 # Trim whitespace from all columns in all.data
-all.data <- all.data %>%
-  mutate(across(everything(), ~str_trim(.)))
+trim_all_columns <- function(data) {
+  data %>%
+    mutate(across(everything(), ~str_trim(.)))
+}
 
-# Get a unique list of cities to clean up
-unique_cities_cleaning <- all.data %>%
-  distinct(city, state, country) %>%
-  arrange(city, state, country) # Arrange to make manual checking easier
-write.csv(unique_cities_cleaning, "unique-cities-cleaning.csv", row.names = FALSE)
+# get a list of unique locations from all the data
+generate_unique_cities_list <- function(data, output_folder) {
+  unique_cities <- data %>%
+    distinct(city, state, country) %>%
+    arrange(city, state, country)
+  write.csv(unique_cities, file.path(output_folder, "unique-cities-cleaning.csv"), row.names = FALSE)
+  return(unique_cities)
+}
 
-## NOTE: I created a new file based on unique_cities_cleaning.csv called unique_cities_replacements.csv and then manually went through and created a new column to clean up typos in the city name
+# use a lookup file of replacements for cleaning up location names - ex. standardizing spellings, fixing typos, etc.
+# the lookup file was manually checked with the second column being the place to manually fix any errors in first column
+# last step is to create a csv of any NEW unique locations that don't exist in the lookup file, then also to add these the end of the replacements file - these will need to be manually checked
+read_and_merge_replacements <- function(unique_cities, output_folder) {
+  existing_replacements <- read.csv(file.path(output_folder, "unique-cities-replacements.csv"), header = TRUE)
+  new_replacements <- unique_cities %>%
+    anti_join(existing_replacements, by = c("city", "state", "country")) %>%
+    mutate(new.city = city) %>%
+    select(city, new.city, everything())
+  all_replacements <- rbind(existing_replacements, new_replacements)
+  write.csv(new_replacements, file.path(output_folder, "unique-cities-replacements-new.csv"), row.names = FALSE)
+  write.csv(all_replacements, file.path(output_folder, "unique-cities-replacements.csv"), row.names = FALSE)
+  return(all_replacements)
+}
+# take the lookup data with replacement names for cities and apply them to the main data
+apply_replacements <- function(data, replacements) {
+  data.cleaned <- data %>%
+    left_join(replacements, by = c("city", "state", "country")) %>%
+    mutate(city = if_else(!is.na(new.city), new.city, city)) %>%
+    select(-new.city)
+  return(data.cleaned)
+  write.csv(data.cleaned, file.path(data_cleaning_folder, "all-data-cleaned.csv"), row.names = FALSE)
+}
 
-# opening up the lookup csv file that has typos and their fixes, then seeing if there are any new values that need to be added to the lookup file
-existing_replacements <- read.csv("unique-cities-replacements.csv", header = TRUE)
-new_replacements <- unique_cities_cleaning %>%
-  anti_join(existing_replacements, by = c("city", "state", "country")) %>%
-  mutate(new.city = city) %>%
-  select(city, new.city, everything())
-write.csv(new_replacements, "unique-cities-replacements-new.csv", row.names = FALSE)
-
-# adding any new values to the end of the existing replacements dataframe and generating a new CSV file (can scroll to end to see new values that might need to be manually cleaned)
-all_replacements <- rbind(existing_replacements, new_replacements)
-write.csv(all_replacements, "unique-cities-replacements.csv", row.names=FALSE)
-
-# Join replacements with all.data and replace city values with the corrected city values 
-all.data.cleaned <- all.data %>%
-  left_join(all_replacements, by = c("city", "state", "country")) %>%
-  mutate(city = if_else(!is.na(new.city), new.city, city)) %>%
-  select(-new.city)
+## call all data cleaning functions
+data_cleaning_folder <- "data-processing-files"
+all.data <- read.csv(file.path(data_cleaning_folder, "all-data-precleaned.csv"))
+all.data <- trim_all_columns(all.data)
+unique_cities <- generate_unique_cities_list(all.data, data_cleaning_folder)
+replacements <- read_and_merge_replacements(unique_cities, data_cleaning_folder)
+all.data.cleaned <- apply_replacements(all.data, replacements)
 
 ### GEOCODING
 
@@ -74,49 +117,59 @@ getGoogleAPI <- function() {
 }
 getGoogleAPI()
 
-# Create a geocode value column (city, state, country)
-all.data.cleaned$geocode.value <- paste(all.data.cleaned$city, ", ", all.data.cleaned$state, ", ", all.data.cleaned$country, sep="")
+## Geocoding function
 
-# Generate a unique list of values you want geocoded
-unique_geocode_values <- unique(all.data.cleaned$geocode.value)
+prep_geocode <- function(data, geocoding_folder) {
+  data$geocode.value <- paste(data$city, ", ", data$state, ", ", data$country, sep="") #create a column for geocoding
+  unique_geocode_values <- unique(data$geocode.value) # Generate a unique list of values you want geocoded
+  existing_geocoded_lookup <- read.csv(file.path(geocoding_folder, "unique-locations-geocoded.csv"), stringsAsFactors = FALSE)
+  # Create dataframes of unique locations that have already been geocoded and unique lcoations that need to be geocoded
+  unique.locations.all <- data.frame(geocode.value = unique_geocode_values)
+  unique.locations.all <- unique.locations.all %>%
+    left_join(existing_geocoded_lookup, by = "geocode.value")
+  unique.locations.already.geocoded <- unique.locations.all %>% 
+    filter(!is.na(lon))
+  unique.locations.to.geocode <- unique.locations.all %>% 
+    filter(is.na(lon))
+  #unique.locations.to.geocode <- unique.locations.to.geocode %>% sample_n(5)
+  print(paste(length(unique.locations.to.geocode$geocode.value), " entries unmatched in unique values that need to be geocoded.", sep = ""))
+  return(unique.locations.to.geocode)
+}
 
-# Load existing CSV file of unique locations that have already been geocoded with their lat/lon info
-existing_geocoded_lookup <- read.csv("unique-locations-geocoded.csv", stringsAsFactors = FALSE)
-
-# Create dataframes of unique locations that have already been geocoded and unique lcoations that need to be geocoded
-unique.locations.all <- data.frame(geocode.value = unique_geocode_values)
-unique.locations.all <- unique.locations.all %>%
-  left_join(existing_geocoded_lookup, by = "geocode.value")
-unique.locations.already.geocoded <- unique.locations.all %>% 
-  filter(!is.na(lon))
-unique.locations.to.geocode <- unique.locations.all %>% 
-  filter(is.na(lon))
-#unique.locations.to.geocode <- unique.locations.to.geocode %>% sample_n(5)
-print(paste(length(unique.locations.to.geocode$geocode.value), " entries unmatched in unique values. Will now be geocoded.", sep = ""))
-
-#geocoding function
-for(i in 1:nrow(unique.locations.to.geocode)) {
+geocoding_function <- function(unique.locations.to.geocode, geocoding_folder) {
+  for(i in 1:nrow(unique.locations.to.geocode)) {
   result <- tryCatch(geocode(unique.locations.to.geocode$geocode.value[i], output = "latlona", source = "google"), warning = function(w) data.frame(lon = NA, lat = NA, address = NA))
   unique.locations.to.geocode$lon[i] <- as.numeric(result[1])
   unique.locations.to.geocode$lat[i] <- as.numeric(result[2])
   unique.locations.to.geocode$geoAddress[i] <- as.character(result[3])
   print(paste("Result: ", toString(result)))
+  }
+  failed_geocode <- unique.locations.to.geocode %>% filter(is.na(lon))
+  write.csv(failed_geocode, file.path(geocoding_folder, "failed-geocode.csv"), row.names = FALSE)
+  return(unique.locations.to.geocode)
 }
 
-# Output a list of locations that failed to geocode
-failed_geocode <- unique.locations.to.geocode %>% filter(is.na(lon))
-write.csv(failed_geocode, "failed-geocode.csv", row.names = FALSE)
+merge_geocode <- function(unique.locations.to.geocode, all.data.cleaned) {
+  # Take succesffully newly geocoded locations and append them to the dataframe of already geocoded lookup locations
+  successful_geocode <- unique.locations.to.geocode %>% filter(!is.na(lon))
+  existing_geocoded_lookup <- read.csv(file.path(geocoding_folder, "unique-locations-geocoded.csv"), stringsAsFactors = FALSE)
+  new_entries <- anti_join(successful_geocode, existing_geocoded_lookup, by = "geocode.value")
+  updated_existing_geocoded_lookup <- bind_rows(existing_geocoded_lookup, new_entries)
+  write.csv(updated_existing_geocoded_lookup, "unique-locations-geocoded.csv", row.names = FALSE)
 
-# Take succesffully newly geocoded locations and append them to the dataframe of already geocoded lookup locations
-successful_geocode <- unique.locations.to.geocode %>% filter(!is.na(lon))
-new_entries <- anti_join(successful_geocode, existing_geocoded_lookup, by = "geocode.value")
-updated_existing_geocoded_lookup <- bind_rows(existing_geocoded_lookup, new_entries)
-write.csv(updated_existing_geocoded_lookup, "unique-locations-geocoded.csv", row.names = FALSE)
+  # Merge the geocoded locations with the entire dataframe of all location records
+  all.data.cleaned$geocode.value <- paste(all.data.cleaned$city, ", ", all.data.cleaned$state, ", ", all.data.cleaned$country, sep="") #create a column for geocoding
+  all.data.cleaned.geocoded <- left_join(all.data.cleaned, updated_existing_geocoded_lookup, by = "geocode.value")
+  output_folder <- "final-output-data"
+  write.csv(all.data.cleaned.geocoded, file.path(output_folder, "all-data-cleaned-geocoded.csv"), row.names = FALSE)
+  return(all.data.cleaned.geocoded)
+}
 
-# Merge the geocoded locations with the entire dataframe of all location records
-all.data.cleaned.geocoded <- left_join(all.data.cleaned, updated_existing_geocoded_lookup, by = "geocode.value")
-output_folder <- "final-output-data"
-write.csv(all.data.cleaned.geocoded, file.path(output_folder, "all-data-cleaned-geocoded.csv"), row.names = FALSE)
+all.data.cleaned <- read.csv(file.path(data_cleaning_folder, "all-data-cleaned.csv"))
+geocoding_folder <- "geocoding-files"
+unique.locations.to.geocode <- prep_geocode(all.data.cleaned, geocoding_folder)
+unique.locations.to.geocode <- geocoding_function(unique.locations.to.geocode, geocoding_folder)
+all.data.cleaned.geocoded<-merge_geocode(unique.locations.to.geocode, all.data.cleaned)
 
 ### RELATIVE DATA CALCULATIONS
 
