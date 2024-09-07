7d6b13d8745f8fbe000567c197160709a425a016
Hallie Knipp <hknipp@g.clemson.edu>
2024-09-07 19:22:11 -0400

    issue #22 -- grid layout and controls to sidebar

--- ui.R
+++ ui.R
@@ -1,24 +1,37 @@
 library(shiny)
 library(plotly)
 library(DT)
+
 navbarPage("GG MGG Explorer", id="nav",       
-    tabPanel("Interactive map",
-        div(class="outer",
-        # If not using custom CSS, set height of leafletOutput to a number instead of percent
-        
-        plotlyOutput("map"),
-                                  
-        checkboxInput("gg", "Gaia's Guide", TRUE),
-        
-        checkboxInput("mgg", "Bob Damron's Address Book", TRUE),
-        
-        checkboxInput("noncontiguous", "Include non-contiguous states", FALSE),
-        
-        selectInput("cityvalue", "Select Value", choices = unique(mgg.gg.data$geocode.value)),
-
-        DTOutput('dtable')
-           ),
-),
+           tabPanel("Interactive map",
+                    div(class="outer",
+                        fluidRow(
+                          column(8,
+                                 div(style = "width: 800px;",  # Set a fixed width for the map
+                                     plotlyOutput("map")
+                                 ),
+                                 div(id="custom_legend",
+                                     style = "margin-top: 20px;",  # Add a top margin to move the legend down
+                                     div(class="legend_item",
+                                         div(class="legend_color", style="background-color: #0F8554; width: 20px; height: 20px; display: inline-block; margin-right: 5px;"),
+                                         div(class="legend_text", "Gaia's Guide", style="display: inline-block;")
+                                     ),
+                                     div(class="legend_item",
+                                         div(class="legend_color", style="background-color: #6F4070; width: 20px; height: 20px; display: inline-block; margin-right: 5px;"),
+                                         div(class="legend_text", "Bob Damron's Address Book", style="display: inline-block;")
+                                     )
+                                 ),
+                                 div(style = "margin-top: 20px;",  # Add a top margin to move the selectInput down
+                                     selectInput("cityvalue", "Select Value", choices = unique(mgg.gg.data$geocode.value))
+                                 )
+                          ),
+                          column(4,
+                                 checkboxInput("gg", "Gaia's Guide", TRUE),
+                                 checkboxInput("mgg", "Bob Damron's Address Book", TRUE),
+                                 checkboxInput("noncontiguous", "Include non-contiguous states", FALSE)
+                          )   
+                        ),
+                        DTOutput('dtable')
+                    )
+           )
 )
-
-#5F4690,#1D6996,#38A6A5,#0F8554,#73AF48,#EDAD08,#E17C05,#CC503E,#94346E,#6F4070,#994E95,#666666
